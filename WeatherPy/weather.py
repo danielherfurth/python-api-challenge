6b8146dc2c59627277b4320e0e7bf48a5b873b9e
danielherfurth <danielherfurth@gmail.com>
2021-10-24 19:40:02 -0400

    saving myself if i fail

--- WeatherPy/weather.py
+++ WeatherPy/weather.py
@@ -1,29 +1,4 @@
-# %%
-"""In this example, you'll be creating a Python script to visualize the weather of 500+ cities across the
-world of varying distance from the equator. To accomplish this, you'll be utilizing a simple Python library, 
-the OpenWeatherMap API, and a little common sense to create a representative model of weather across world cities.
-
-The first requirement is to create a series of scatter plots to showcase the following relationships:
-
-Temperature (F) vs. Latitude
-Humidity (%) vs. Latitude
-Cloudiness (%) vs. Latitude
-Wind Speed (mph) vs. Latitude
-After each plot, add a sentence or two explaining what the code is analyzing.
-
-The second requirement is to run linear regression on each relationship. This time, separate the plots into Northern
-Hemisphere (greater than or equal to 0 degrees latitude) and Southern Hemisphere (less than 0 degrees latitude):
-
-Northern Hemisphere - Temperature (F) vs. Latitude
-Southern Hemisphere - Temperature (F) vs. Latitude
-Northern Hemisphere - Humidity (%) vs. Latitude
-Southern Hemisphere - Humidity (%) vs. Latitude
-Northern Hemisphere - Cloudiness (%) vs. Latitude
-Southern Hemisphere - Cloudiness (%) vs. Latitude
-Northern Hemisphere - Wind Speed (mph) vs. Latitude
-Southern Hemisphere - Wind Speed (mph) vs. Latitude
-"""
-
+#!
 # region
 # Dependencies and Setup
 import matplotlib.pyplot as plt
@@ -31,6 +6,9 @@ import pandas as pd
 import numpy as np
 import requests, json, time
 from scipy.stats import linregress
+from citipy import citipy  # Incorporated citipy to determine city based on latitude and longitude
+# Import API key
+from config import weather_api_key
 
 
 def k_to_c(temp):
@@ -45,18 +23,9 @@ def north_or_south(latitude):
     return latitude < 0
 
 
-# %%
-# Import API key
-from config import weather_api_key
-
 api_key = weather_api_key
-
-# Incorporated citipy to determine city based on latitude and longitude
-from citipy import citipy
-
 # endregion
 
-# %%
 # Output File (CSV)
 output_file = "output_data/cities.csv"
 
@@ -80,11 +49,10 @@ for lat_lng in lat_lngs:
 
 len(cities)
 
-# %%
 temp_dict = {}
 
 ol = []
-i = 0
+i, j = 0, 0
 
 for city in cities:
     search_str = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}'
@@ -100,67 +68,7 @@ for city in cities:
             ]
             i += 1
             temp_dict[city] = vs
-
-# %%
-temp_dict = {}
-i = 0
-
-for city in cities:
-    if i < 500:
-        search_str = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}'
-        resp = requests.get(search_str)
-        if resp.status_code == 200:
-            temp_k = resp.json()['main']['temp']
-            temp_c = k_to_c(temp_k)
-            temp_dict[city] = temp_c
-            print(f'Request {i}: {city} successfully acquired.')
-            i += 1
-        else:
-            print(f'{city} received 404.')
-
-# %%
-# search_str = f'http://api.openweathermap.org/data/2.5/weather?q={cities[0]}&appid={api_key}'
-# resp = requests.get(search_str).json()  # ['main']['temp']
-# print(resp)
-# print(k_to_c(resp))
-
-# %%
-# i = 0
-# for city in cities[:100]:
-#     if i < 520:
-#         if i % 2 == 0:
-#             print(city)
-#         i += 1
-#     else:
-#         break
-
-# %%
-# l = 'coord main visibility'.split()
-# temp_dict = {}
-#
-# # for val in l:
-# #     temp_dict[val] = resp[val]
-#
-#
-# 'coord temp humidity clouds wind_speed'
-
-# %%
-# temp_dict={}
-#
-# ol = []
-#
-#
-# for city in cities[0:3]:
-#     search_str = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}'
-#     resp = requests.get(search_str).json()
-#
-#     vs = [
-#             resp['coord']['lat'],
-#             k_to_c(resp['main']['temp']),
-#             resp['main']['humidity'],
-#             kmh_to_mph(resp['wind']['speed']),
-#     ]
-#
-#     temp_dict[city] = vs
-#
-# print(temp_dict)
+        print(f'Request {j}: {city} request successful.')
+    else:
+        print(f'Request {j}: {city} request failed.')
+    j += 1
